4018a30d1ad51ed05eaaaeb7c610f815733cd6b0
0-tae <100774819+0-tae@users.noreply.github.com>
2023-07-28 15:04:34 +0900

    Initial commit

---
+++ src/test/java/com/choi0tae/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choi0tae.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
